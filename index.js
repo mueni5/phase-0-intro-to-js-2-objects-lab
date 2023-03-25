21a39a04ca163598e6c86d1eaafb01b920001a62
+ <+>
2023-03-25 12:16:39 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Write your solution in this file!
+
+const employee = {
+    name: "",
+    streetAddress: "",
+  };
+
+  employee.name = "entername";
+employee.streetAddress = "enteraddy";
+employee;
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+  const newEmployee = { ...employee };
+  newEmployee[key] = value;
+
+  return newEmployee;
+}
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee["name"] = "Sam";
+    employee["streetAddress"] = "12 Broadway";
+    return employee;
+  }
+
+  function deleteFromEmployeeByKey(employee, key) {
+    const newEmployee = { ...employee };
+    delete newEmployee.name;
+    return newEmployee;
+  }
+
+  function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee["name"];
+    return employee;
+  }
+  
+  destructivelyDeleteFromEmployeeByKey();
